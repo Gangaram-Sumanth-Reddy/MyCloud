ef6df353bc899dc17ed10f77ae3f4ef5e9dd4603
Gangaram Sumanth Reddy <gsumanthreddy2004@gmail.com>
2026-02-18 15:22:30 +0530

    fix: allow Render host in Vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   cacheDir: '.vite',
+
   server: {
     port: 5173,
     strictPort: false,
   },
+
+  preview: {
+    allowedHosts: [
+      'cloud-storage-frontend-o4io.onrender.com'
+    ]
+  }
 })
